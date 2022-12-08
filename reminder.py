158158906ce441f2b930a60a788f4ae5d0dba6b7
jayencj <jayencj14@gmail.com>
2022-12-07 17:03:23 -0800

    made sort work
    dont know how or why

--- reminder.py
+++ reminder.py
@@ -473,16 +473,17 @@ def tick():
 
 	templst = selection_sort(veryimportanttasks,len(veryimportanttasks))
 
-	veryimportanttasks.clear()
-
-	veryimporanttasks = templst
-
-	for event in veryimportanttasks:
+	for event in templst:
 
 		print(event.datetime)
 		print(event.title)
-		print()
 
+
+	#veryimportanttasks.clear()
+
+	#veryimporanttasks = templst
+
+	
 
 	#i=0
 	#for i in range(0, len(templst)):
@@ -745,8 +746,52 @@ def tick():
 		tasks_shown = [i for j, i in enumerate(tasks_shown) if j not in to_be_deleted]
 
 	clockTime.after(100,tick)
-	
 
+def selection_sort(unsorted, n):
+	
+	sortedlst=unsorted
+
+	def swap(arr, a, b):
+	    """ swap elements a and b in an array """
+	    print("-----------------SwapRan-------------------")
+	    temp = arr[a]
+	    arr[a] = arr[b]
+	    arr[b] = temp
+	    return(arr)
+
+	CurrentTime = time.strftime("%m:%d:%H:%M")
+	datetimeCurrent = datetime.strptime(CurrentTime, "%m:%d:%H:%M")
+
+	lstsorted = False
+
+	for i in range(0, n):
+		print("------------------------------selectionSortForLoopRan-----------------------------------")
+
+		check_swap = False
+
+		# initialise with first value
+		current_min = (unsorted[i].datetime - datetimeCurrent).total_seconds()
+		
+		# min_index initialiser
+		min_index = i
+		
+		# iterate over remaining unsorted items
+		for j in range(i, n):
+			
+			# check if jth value is less than current min
+			if (unsorted[j].datetime - datetimeCurrent).total_seconds() < current_min:
+			  
+				# update minimum value and index
+				current_min = (unsorted[j].datetime - datetimeCurrent).total_seconds()
+				min_index = j
+				check_swap = True
+
+		# swap ith and jth values
+		sortedlst = swap(unsorted, i, min_index)
+
+	return(sortedlst)
+	
+'''
 def selection_sort(unsorted, n):
 	
 	def swap(arr, a, b):
@@ -789,7 +834,7 @@ def selection_sort(unsorted, n):
 		sortedlst.append(swap(unsorted, i, min_index))
 
 	return(sortedlst)
-
+'''
 
 def remove_by_name(item_to_remove):
 
